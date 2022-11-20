5e09216b2be24b3190d3245fccc62e6735a7787e
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2022-11-20 15:36:39 -0300

    feat: :recycle: Atualização de código para lógica de repo dinamico

--- src/pages/Forms/FormsPage.jsx
+++ src/pages/Forms/FormsPage.jsx
@@ -1,14 +1,17 @@
 import { FormsPageStyle } from "./FormsPageStyle";
 import { questions } from "../../utils/constants";
 import Forms from "../../components/Forms/Forms";
+import { useState } from "react";
 
 export default function FormsPage(){
-    const values = Object.values(questions[0])
+
+    const [questionNuber, setQuestionNumber] = useState(0)
+    const values = Object.values(questions[questionNuber])
     values.shift()
-    const description = questions[0].description
+    const description = questions[questionNuber].description
     return(
         <FormsPageStyle>
-            <Forms question={description} possibilitys={values} ></Forms>
+            <Forms question={description} possibilitys={values} questionNumber={questionNuber} setQuestionNumber={setQuestionNumber} ></Forms>
         </FormsPageStyle>
     )
 }
