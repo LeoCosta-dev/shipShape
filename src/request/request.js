f528096eda2357e184a6c74a5e88015462fac07a
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2022-11-20 22:19:43 -0300

    refactor: Adição de slogan

--- src/request/request.js
+++ src/request/request.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import Amadeus from "amadeus";
+
 
 
 export async function requestCity(payload){
@@ -13,13 +13,3 @@ export async function requestCity(payload){
     }
 }
 
-export async function requestCovid(countryCode){
-    const amadeus = new Amadeus({
-        clienteId: "AlASor8p1FD2tQxlsdOfYzIoq9PkLuYs",
-        clientSecret: "pGfaxt5EAG64GlSJ"
-    })
-    const url = `v2/duty-of-care/diseases/covid19-area-report?countryCode=${countryCode}`
-    const response = await amadeus.client.get()
-}
-
-requestCovid("ES").then(response => console.log(response))
