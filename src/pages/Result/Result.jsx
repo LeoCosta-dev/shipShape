98b04b028d328468a69a29be66ec0126d24a1b25
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2022-11-20 21:52:36 -0300

    feat: Adição de navegabilidade do prorótipo

--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -2,20 +2,23 @@ import Button from '../../components/Button/Button';
 import { Link } from 'react-router-dom';
 import { ResultStyle } from './ResultStyle';
 import aviao  from "../../assets/Files/aviao.jpg"
+import { NumberQuestionContext } from '../../context/NumberQuestionContext';
+import { useContext } from 'react';
 
 
-export default function Results (props){
+export default function Results ({nome_da_cidade, risco_covid}){
+    const { numberQuestion } = useContext(NumberQuestionContext)
 
     return (
         <ResultStyle>
             <h1>Seu destino perfeito é</h1>
             <img src={aviao} alt='Imagem de avião em cor laranja' />
-            <h1>{props.nome_da_cidade}</h1>
-            <h1>Risco COVID: {props.risco_covid}</h1>
+            <h1>{numberQuestion === 6 ? "Barcelona" : "Nova Iorque"}</h1>
+            <h1>Risco COVID: {numberQuestion === 6 ? "Moderate" : "Medium"}</h1>
 
             <div>
-                <Button link={'https://www.youtube.com/watch?v=s1JPZZdzCOE%27%7D'} texto={'Tour pelo metaverso'}/>
-                <Link to='/'><Button texto={'Tentar novamente'} /></Link>
+                <Button link={'https://www.youtube.com/watch?v=s1JPZZdzCOE%27%7D'} texto={'Tour pelo metaverso'} target={"blank"} />
+                <Link to={numberQuestion <= 6 ? '/questions' : '/'}><Button texto={'Tentar novamente'} /></Link>
             </div>
         </ResultStyle>
     );
